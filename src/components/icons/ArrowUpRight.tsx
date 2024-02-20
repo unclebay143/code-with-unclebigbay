513c1a9e9226af9c19b316669d567c0bfde28601
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-02-20 13:05:10 +0100

    Sam/setup navbar (#1)
    * Chore
    
    * Add courses and video section
    
    * Feat: add testimonial and community members section
    
    * Lint fix: add display name to IconButton component

---
+++ src/components/icons/ArrowUpRight.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type Props = {
+  className: string;
+};
+
+export const ArrowUpRight = (props: Props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={props.className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+      />
+    </svg>
+  );
+};
---
+++ src/components/ui/SlideOver.tsx
@@ -0,0 +1,112 @@
+import React, { createContext } from 'react';
+import * as SheetPrimitive from '@radix-ui/react-dialog';
+import { cva } from 'class-variance-authority';
+import { XMark } from '../icons/XMark';
+import { IconButton } from './IconButton';
+
+const slideOverContent = cva(
+  'z-[9999] flex flex-col w-full sm:w-[460px] fixed inset-y-0 right-0 bg-white dark:bg-slate-950',
+);
+
+const slideOverOverlay = cva(
+  'fixed z-[9999] inset-0 opacity-50 bg-slate-900 dark:bg-slate-600',
+);
+
+const slideOverHeader = cva(
+  'flex items-center justify-between pt-4 px-5 pb-3 border-b border-slate-200 dark:border-slate-800/80',
+  {
+    variants: {
+      border: { none: ['border-b-transparent'], visible: ['border'] },
+    },
+  },
+);
+
+const slideOverContext = createContext({
+  isOpen: false,
+  closeSlideOver: () => {},
+});
+
+const useSlideOver = () => {
+  const context = React.useContext(slideOverContext);
+  if (context === undefined) {
+    throw new Error('useSlideOver must be used within a SlideOverProvider');
+  }
+  return context;
+};
+
+const SlideOverProvider = ({
+  children,
+  isOpen,
+  closeSlideOver,
+}: {
+  children: React.ReactNode;
+  isOpen: boolean;
+  closeSlideOver: () => void;
+}) => {
+  return (
+    <slideOverContext.Provider value={{ isOpen, closeSlideOver }}>
+      {children}
+    </slideOverContext.Provider>
+  );
+};
+
+type SlideOverProps = {
+  isOpen: boolean;
+  closeSlideOver: () => void;
+  children?: React.ReactNode;
+  asChild?: boolean;
+};
+
+export const SlideOverHeader = ({
+  title,
+  borderless,
+}: {
+  title?: string;
+  borderless?: boolean;
+}) => {
+  const { closeSlideOver } = useSlideOver();
+  return (
+    <div
+      className={slideOverHeader({
+        border: borderless ? 'none' : 'visible',
+      })}
+    >
+      <SheetPrimitive.Title className="font-semibold text-xl text-slate-700 dark:text-slate-200">
+        {title}
+      </SheetPrimitive.Title>
+      <SheetPrimitive.Close asChild>
+        <IconButton
+          size="md"
+          onClick={closeSlideOver}
+          aria-label="Close"
+          Icon={XMark}
+        />
+      </SheetPrimitive.Close>
+    </div>
+  );
+};
+
+export const SlideOver = ({
+  isOpen,
+  closeSlideOver,
+  children,
+  asChild,
+}: SlideOverProps) => (
+  <SlideOverProvider isOpen={isOpen} closeSlideOver={closeSlideOver}>
+    <SheetPrimitive.Root open={isOpen}>
+      <SheetPrimitive.Portal>
+        <SheetPrimitive.Overlay
+          onClick={closeSlideOver}
+          className={slideOverOverlay()}
+        />
+        <SheetPrimitive.Content
+          asChild={asChild}
+          onEscapeKeyDown={closeSlideOver}
+          className={slideOverContent()}
+        >
+          {children}
+        </SheetPrimitive.Content>
+      </SheetPrimitive.Portal>
+    </SheetPrimitive.Root>
+  </SlideOverProvider>
+);
