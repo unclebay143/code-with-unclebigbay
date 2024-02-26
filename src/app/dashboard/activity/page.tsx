9cecfd056ebd174a64bfd62adf6c26f04a18b159
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-02-27 00:01:07 +0100

    Sam/course UI (#27)
    * Refactor: dashboard layout
    
    * Refactor: dashboard layout
    
    * Feat: add activity log section
    
    * Feat: add courses page design
    
    * Design: misc fixes

---
+++ src/app/dashboard/activity/page.tsx
@@ -0,0 +1,7 @@
+import { ActivityLogs } from '@/components/dashboard/activity-logs';
+import React from 'react';
+
+const Page = () => {
+  return <ActivityLogs defaultCount={15} />;
+};
+export default Page;
