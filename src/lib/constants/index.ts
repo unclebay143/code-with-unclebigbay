7393439cd46fdd93f69a82c58104fa6f88b99ae2
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-02-27 13:22:47 +0100

    Sam/design patches 1 (#30)
    * Refactor: dashboard layout
    
    * Refactor: dashboard layout
    
    * Feat: add activity log section
    
    * Feat: add courses page design
    
    * Design: misc fixes
    
    * Design: patches
    
    * Feat: add leaderboard page

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -6,4 +6,99 @@ export const communityMember = {
   username: 'unclebigbay',
   photo:
     'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+  totalScore: 10,
+  flag: '🇦🇺',
 };
+
+export const communityMembers = [
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: '',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: '',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+  {
+    name: 'Ayodele S. Adebayo',
+    stack: 'frontend',
+    username: 'unclebigbay',
+    photo:
+      'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
+    totalScore: 10,
+    flag: '🇦🇺',
+  },
+];
