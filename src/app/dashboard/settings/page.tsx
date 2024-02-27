3cbf1f21a59aaa20b36d4490d327a6745ab0f6b8
eedrisofficial <eedrisofficial.gmail.com>
2024-02-27 12:03:20 -0500

    design implementtion for user-profil-setting

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserSettings from '@/components/dashboard/user-settings';
+
+const page = () => {
+  return (
+    <div>
+      <UserSettings />
+    </div>
+  );
+};
+
+export default page;
