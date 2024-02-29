2d23f1ada787e80e452f505fd6da8c4d3dae1ab7
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-02-29 12:38:32 +0100

    Feat: add help centres page (#38)

---
+++ src/app/dashboard/help-centres/page.tsx
@@ -0,0 +1,10 @@
+import { WhiteArea } from '@/components/dashboard/white-area';
+import React from 'react';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return <WhiteArea border>Help centres</WhiteArea>;
+};
+
+export default Page;
