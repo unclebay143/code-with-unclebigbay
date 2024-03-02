18ee4b83d1d619aa16fbeea3b046bd2e4298693b
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-03-02 14:23:36 +0100

    Feat: Add course collection

--- src/models/course.ts
+++ src/models/course.ts
@@ -1,5 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose, { model, models } from 'mongoose';
 
 const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({});
+
+const Course = models.course || model('course', courseSchema);
+
+export { Course };
