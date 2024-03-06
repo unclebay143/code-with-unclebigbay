7bfb785003084c2ecbe2fea5b18e9b745c8d5566
eedrisofficial <eedrisofficial.gmail.com>
2024-03-06 04:41:39 -0500

    correction on previous work effected and  user-settings splot into components

--- src/validation/userSocialValidation.ts
+++ src/validation/userSocialValidation.ts
@@ -4,41 +4,56 @@ export const ProfileUpdateSchema = z.object({
   website: z
     .string()
     .url()
-    .includes('https://', { message: 'Invalid web address' }),
+    .includes('https://', { message: 'Invalid web address' })
+    .or(z.literal('')),
   github: z
     .string()
     .url()
-    .includes('https://github.com/', { message: 'Invalid GitHub URL' }),
+    .includes('https://github.com/', { message: 'Invalid GitHub URL' })
+    .or(z.literal('')),
   facebook: z
     .string()
     .url()
-    .includes('https://facebook.com/', { message: 'Invalid facebook URL' }),
+    .includes('https://facebook.com/', { message: 'Invalid facebook URL' })
+    .or(z.literal('')),
   twitter: z
     .string()
     .url()
-    .includes('https://twitter.com/', { message: 'Invalid twitter URL' }),
+    .includes('https://twitter.com/', { message: 'Invalid twitter URL' })
+    .or(z.literal('')),
   linkedIn: z
     .string()
     .url()
-    .includes('https://linkedIn.com/', { message: 'Invalid linkedIn URL' }),
+    .includes('https://linkedIn.com/', { message: 'Invalid linkedIn URL' })
+    .or(z.literal('')),
   instagram: z
     .string()
     .url()
-    .includes('https://instagram.com/', { message: 'Invalid instagram URL' }),
-  hashnode: z
-    .string()
-    .url()
-    .includes('https://hashnode.com/', { message: 'Invalid hashnode URL' }),
+    .includes('https://instagram.com/', { message: 'Invalid instagram URL' })
+    .or(z.literal('')),
+
   stackoverflow: z
     .string()
     .url()
-    .includes('https://', { message: 'Invalid stackoverflow URL' }),
+    .includes('https://', { message: 'Invalid stackoverflow URL' })
+    .or(z.literal('')),
   youtube: z
     .string()
     .url()
-    .includes('https://youtube.com/', { message: 'Invalid youtube URL' }),
+    .includes('https://youtube.com/', { message: 'Invalid youtube URL' })
+    .or(z.literal('')),
   mastodon: z
     .string()
     .url()
-    .includes('https://', { message: 'Invalid mastodon URL' }),
+    .includes('https://', { message: 'Invalid mastodon URL' })
+    .or(z.literal('')),
+});
+
+export const professionalDetailSchema = z.object({
+  portfolio: z.string().url().or(z.literal('')),
+  blog: z.string().url().or(z.literal('')),
+});
+
+export const personalDetailSchema = z.object({
+  bio: z.string().or(z.literal('')),
 });
