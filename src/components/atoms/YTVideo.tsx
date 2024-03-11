5242f3832fa7acfbaf4772117a45455c527a7806
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-03-11 23:19:49 +0100

    Feat: add material fetch API route

--- src/components/atoms/YTVideo.tsx
+++ src/components/atoms/YTVideo.tsx
@@ -1,9 +1,9 @@
-export function YTVideo({ embedURL }: { embedURL: string }) {
+export function YTVideo({ ytVideoId }: { ytVideoId: string }) {
   return (
     <div className="aspect-video">
       <iframe
         className="w-full h-full"
-        src={embedURL}
+        src={`https://www.youtube.com/embed/${ytVideoId}`}
         title="Learn How to Code in 2024 | Become a Web Developer in 2024 - Your Roadmap Starts Here"
         frameBorder={0}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
