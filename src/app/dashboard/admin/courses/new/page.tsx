3af1c35e272229f2bff15fb26d644f3fcb726523
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-03-12 20:03:50 +0100

    Update: Add tag-related functionalities to material fetching routes. Improve tag selection UI for course creation.

--- src/app/dashboard/admin/courses/new/page.tsx
+++ src/app/dashboard/admin/courses/new/page.tsx
@@ -3,15 +3,24 @@
 import { Button } from '@/components/atoms/Button';
 import useCurrentStudent from '@/components/hooks/useCurrentStudent';
 import useMaterial from '@/components/hooks/useMaterial';
+import useTag from '@/components/hooks/useTag';
 import { AddAssignmentModal } from '@/components/molecules/dashboard/add-assignment-modal';
 import { DashboardSubheading } from '@/components/molecules/dashboard/dashboard-subheading';
 import { WhiteArea } from '@/components/molecules/dashboard/white-area';
-import { Material, Questions } from '@/utils/types';
+import { convertWhiteSpaceToDash } from '@/utils';
+import { Material, Questions, Tag, Tags } from '@/utils/types';
+import { X } from 'lucide-react';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 
 const Page = () => {
+  const { tags, mutation: createTag } = useTag();
+  const [showSuggestedTags, setShowSelectedTags] = useState(false);
+  const [selectTags, setSelectedTags] = useState<Tags>([]);
+  const [currentTag, setCurrentTag] = useState<string>('');
+
   const { mutation } = useMaterial();
   const [selectedQuestions, setSelectedQuestions] = useState<Questions>([]);
   const [openAssignmentModal, setOpenAssignmentModal] =
@@ -37,8 +46,75 @@ const Page = () => {
       ...data,
       type: 'video',
       assignment: selectedQuestions.map((question) => question._id),
+      tags: selectTags.map((tag) => tag._id),
     };
     mutation.mutate(newCourse);
+  };
+
+  const SuggestedTags = () => {
+    if (!currentTag && !showSuggestedTags) return;
+
+    const excludeSelectedTags = tags?.filter(
+      (tag) => !selectTags.some((selectTag) => selectTag._id === tag._id),
+    );
+
+    const filteredTags = excludeSelectedTags
+      ?.map((tag) => {
+        if (tag.name.toLowerCase().includes(currentTag.toLowerCase()))
+          return tag as Tag;
+      }) // array contains undefined for which doesn't exist
+      .filter((tag) => tag !== undefined); // filter out undefineds
+
+    return (
+      <div className="absolute right-0 w-full h-auto bg-white rounded-md shadow-lg top-100 dark:bg-slate-800">
+        {currentTag && filteredTags && filteredTags?.length < 1 && (
+          <button
+            type="button"
+            className="capitalize text-sm text-slate-600 font-medium w-full text-left px-4 py-3 hover:bg-slate-50"
+            onClick={() => {
+              createTag.mutate({
+                name: currentTag,
+                slug: convertWhiteSpaceToDash(currentTag),
+              });
+            }}
+          >
+            Note found. Create{' '}
+            <span className="font-semibold text-green-800 lowercase">
+              &quot;{currentTag}&quot;
+            </span>{' '}
+            as a tag
+          </button>
+        )}
+
+        {filteredTags?.map((tag) => {
+          if (!tag) return;
+          return (
+            <button
+              key={tag?._id}
+              type="button"
+              className="lowercase text-sm text-slate-600 font-medium w-full text-left px-4 py-3 hover:bg-slate-50"
+              onClick={() => {
+                const alreadyExist = selectTags.some(
+                  ({ name }) => name === currentTag.toLowerCase(),
+                );
+
+                if (alreadyExist) {
+                  toast.success('Tag already selected');
+                  setCurrentTag('');
+                  return;
+                }
+
+                setSelectedTags((prevTags) => [...prevTags, tag as Tag]);
+                setCurrentTag('');
+                setShowSelectedTags(false);
+              }}
+            >
+              {tag?.name}
+            </button>
+          );
+        })}
+      </div>
+    );
   };
 
   return (
@@ -125,6 +201,58 @@ const Page = () => {
                     placeholder="3600"
                     className={`text-sm text-slate-600 p-2 outline-none focus:ring-2 focus:ring-slate-500 focus:border-slate-300 border rounded-md`}
                   />
+                </div>
+                <div className="relative">
+                  <div className="flex flex-col gap-2">
+                    <div className="text-slate-600">
+                      <label htmlFor="tags" className="text-slate-600 text-sm">
+                        <DashboardSubheading title="Select tags" />
+                      </label>
+                    </div>
+                    <div className="text-slate-600">
+                      <input
+                        autoComplete="off"
+                        type="text"
+                        id="tags"
+                        placeholder="Search or create tag"
+                        className="w-full text-sm text-slate-600 p-2 outline-none focus:ring-2 focus:ring-slate-500 focus:border-slate-300 border rounded-md"
+                        value={currentTag}
+                        onFocus={() => setShowSelectedTags(true)}
+                        onChange={(e) => {
+                          const inputValue = e.target.value;
+                          let cleanedInputValue = inputValue.replace(
+                            /\s/g,
+                            '-',
+                          ); // remove space
+                          setCurrentTag(cleanedInputValue);
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <SuggestedTags />
+                  <div className="flex flex-wrap gap-1 pt-1">
+                    {selectTags.map(({ name }) => {
+                      return (
+                        <div className="flex items-center" key={name}>
+                          <span className="bg-slate-50 text-xs rounded font-medium px-1 lowercase">
+                            {name}
+                          </span>
+                          <button
+                            type="button"
+                            onClick={() => {
+                              const removedTag = selectTags.filter(
+                                (prevTag) => prevTag.name !== name,
+                              );
+                              setSelectedTags(removedTag);
+                            }}
+                            className="text-red-500"
+                          >
+                            <X className="h-3 w-3" />
+                          </button>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
                 <div>
                   <Button size="xs" type="submit">
