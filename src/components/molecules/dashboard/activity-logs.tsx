0738b1d14b42817d7f46ff8bf53f8343d9d8e87a
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-03-16 15:28:50 +0100

    Feat: add activity log loader

--- src/components/molecules/dashboard/activity-logs.tsx
+++ src/components/molecules/dashboard/activity-logs.tsx
@@ -7,6 +7,72 @@ import { DashboardSubheading } from './dashboard-subheading';
 import { Button } from '../../atoms/Button';
 import useAudit from '@/components/hooks/useAudit';
 
+const renderTitle = (title: string) => (
+  <h3 className="text-slate-600 ">{title}</h3>
+);
+
+const AuditTimeLine = ({
+  time,
+  title,
+  description,
+  url,
+}: {
+  url?: string;
+  time?: string;
+  title: string;
+  description?: string;
+}) => {
+  return (
+    <div className="relative pl-8 sm:pl-32 py-6 group">
+      {/* {type && (
+                    <div className="font-medium text-slate-600 mb-1 sm:mb-0">
+                      {type}
+                    </div>
+                  )} */}
+      {/* Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) */}
+      <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-slate-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+        <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-slate-600 bg-slate-100 rounded-full">
+          {new Date(time!).toLocaleString('USA', {
+            day: 'numeric',
+            month: 'short',
+            year: '2-digit',
+          })}
+        </time>
+        {url ? (
+          <Link href={url} className="hover:underline">
+            {renderTitle(title)}
+          </Link>
+        ) : (
+          renderTitle(title)
+        )}
+      </div>
+      {description && <div className="text-slate-500">{description} </div>}
+    </div>
+  );
+};
+
+const AuditTimeLineLoader = ({ count = 2 }: { count?: number }) => {
+  return (
+    <>
+      {Array(count)
+        .fill({})
+        .map(() => {
+          return (
+            <>
+              <div className="relative pl-8 sm:pl-32 py-6 group">
+                <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-slate-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                  <time className="sm:absolute left-0 translate-y-0.5 w-20 h-6 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
+                  <time className="w-28 h-6 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
+                </div>
+                <div className="w-24 h-5 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
+              </div>
+            </>
+          );
+        })}
+    </>
+  );
+};
+
 export const ActivityLogs = ({
   defaultCount = 5,
   hideShowMore,
@@ -14,57 +80,41 @@ export const ActivityLogs = ({
   defaultCount?: number;
   hideShowMore?: boolean;
 }) => {
-  const { audits } = useAudit();
+  const { audits, isFetching } = useAudit();
 
-  const renderTitle = (title: string) => (
-    <h3 className="text-slate-600 ">{title}</h3>
-  );
   return (
     <WhiteArea border>
       <div className="flex flex-col gap-5">
         <DashboardSubheading title="Activity" />
-        <div className="-my-6">
-          {audits
-            ?.slice(0, defaultCount)
-            .map(({ _id, createdAt, title, description, url }) => {
-              return (
-                <div className="relative pl-8 sm:pl-32 py-6 group" key={_id}>
-                  {/* {type && (
-                    <div className="font-medium text-slate-600 mb-1 sm:mb-0">
-                      {type}
-                    </div>
-                  )} */}
-                  {/* Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) */}
-                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-slate-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-slate-600 bg-slate-100 rounded-full">
-                      {new Date(createdAt!).toLocaleString('USA', {
-                        day: 'numeric',
-                        month: 'short',
-                        year: '2-digit',
-                      })}
-                    </time>
-                    {url ? (
-                      <Link href="" className="hover:underline">
-                        {renderTitle(title)}
-                      </Link>
-                    ) : (
-                      renderTitle(title)
-                    )}
-                  </div>
-                  {description && (
-                    <div className="text-slate-500">{description} </div>
-                  )}
-                </div>
-              );
-            })}
-          {!hideShowMore && (
-            <div className="relative pl-8 sm:pl-32 py-6 flex items-start">
-              <Button size="sm" appearance="secondary-slate" asChild>
-                <Link href="/dashboard/activity">Show more</Link>
-              </Button>
-            </div>
-          )}
-        </div>
+
+        {isFetching ? (
+          <div className="-my-6">
+            <AuditTimeLineLoader />
+          </div>
+        ) : (
+          <div className="-my-6">
+            {audits
+              ?.slice(0, defaultCount)
+              .map(({ _id, createdAt, title, description, url }) => {
+                return (
+                  <AuditTimeLine
+                    key={_id}
+                    time={createdAt}
+                    title={title}
+                    description={description}
+                    url={url}
+                  />
+                );
+              })}
+            {!hideShowMore && (
+              <div className="relative pl-8 sm:pl-32 py-6 flex items-start">
+                <Button size="sm" appearance="secondary-slate" asChild>
+                  <Link href="/dashboard/activity">Show more</Link>
+                </Button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </WhiteArea>
   );
