6ca3507667af61794ff5665581d71c5844647346
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-03-20 16:20:36 +0100

    Refactor: enrolment and course view

--- src/models/enroll.ts
+++ src/models/enroll.ts
@@ -1,11 +1,18 @@
 import mongoose, { model, models } from 'mongoose';
 const Schema = mongoose.Schema;
 
-const enrollSchema = new Schema({
-  course: { type: Schema.Types.ObjectId, ref: 'Course' },
-  student: { type: Schema.Types.ObjectId, ref: 'Student' },
-  enrolledDate: { type: Date, default: Date.now },
-});
+const enrollSchema = new Schema(
+  {
+    course: { type: Schema.Types.ObjectId, ref: 'Course' },
+    student: { type: Schema.Types.ObjectId, ref: 'Student' },
+    enrolledDate: { type: Date, default: Date.now },
+    isCompleted: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+    collection: 'students',
+  },
+);
 
 const Enroll = models.Enroll || model('Enroll', enrollSchema);
 
