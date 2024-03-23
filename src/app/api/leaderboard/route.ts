cbdeb4a69b2a77f1a35b11c5fff37d2179561de9
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-03-23 10:30:49 +0100

    Fix: course id returning undefined

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -4,6 +4,51 @@ import { getServerSessionWithAuthOptions } from '@/utils/auth-options';
 import connectViaMongoose from '@/utils/mongoose';
 import { NextResponse } from 'next/server';
 
+// const POST = async (req: Request, _res: Response) => {
+//   const body = await req.json();
+//   try {
+//     await connectViaMongoose();
+
+//     const { studentId, newScore } = body;
+//     if (studentId || newScore) {
+//       return NextResponse.json(
+//         { message: 'studentId or newScore required' },
+//         {
+//           status: 404,
+//         },
+//       );
+//     }
+
+//     const updatedLeaderboard = await LeaderBoard.findOneAndUpdate(
+//       {
+//         student: studentId,
+//       },
+//       {
+//         $inc: { totalScore: newScore },
+//       },
+//       { new: true },
+//     );
+
+//     return NextResponse.json(
+//       {
+//         message: 'Leaderboard updated.',
+//         updatedLeaderboard,
+//       },
+//       {
+//         status: 200,
+//       },
+//     );
+//   } catch (e: any) {
+//     return NextResponse.json(
+//       { error: e.message },
+
+//       {
+//         status: 500,
+//       },
+//     );
+//   }
+// };
+
 const GET = async () => {
   try {
     await connectViaMongoose();
@@ -81,13 +126,6 @@ const GET = async () => {
       },
     );
   }
-};
-
-const POST = async () => {
-  const body = await req.json();
-  try {
-    await connectViaMongoose();
-  } catch (error) {}
 };
 
 export { GET };
