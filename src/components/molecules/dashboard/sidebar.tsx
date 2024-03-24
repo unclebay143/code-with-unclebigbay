22c2b9e19bbcc6f0c9d73790dccfbbf72a4b7194
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-03-24 09:47:30 +0100

    Merge mobile and desktop sidebar to one render

--- src/components/molecules/dashboard/sidebar.tsx
+++ src/components/molecules/dashboard/sidebar.tsx
@@ -40,143 +40,215 @@ export const Sidebar = ({
   isAdmin,
   isLoggedIn,
   isOnboardingCompleted,
+  sidebarOpen,
+  setSidebarOpen,
 }: {
   isAdmin?: boolean;
   isLoggedIn?: boolean;
   isOnboardingCompleted?: boolean;
+  sidebarOpen: boolean;
+  setSidebarOpen: Function;
 }) => {
   const pathname = usePathname();
   const currentPageName = pathname.split('/')[2] || pathname.split('/')[1];
 
   return (
-    <aside className="hidden lg:flex">
-      <nav>
-        <div className="transition-all w-0 lg:w-[270px]" />
-        <div className="border rounded-lg flex flex-col gap-1 px-2 fixed py-4 z-10 bg-white dark:bg-slate-950 w-[270px]">
-          {sidebarLinks.map(
-            ({
-              key,
-              label,
-              Icon,
-              slug,
-              shadowHide,
-              adminAccess,
-              requireAuth,
-              showOnBoard,
-              hideAfterOnboard,
-            }) => {
-              const isCurrentPage = currentPageName === key.toLowerCase();
-              const requireAdminAccess = adminAccess && !isAdmin;
-              const shadowHidden = shadowHide && !isCurrentPage;
-              const hideTillOnboard = !isOnboardingCompleted && !showOnBoard;
-              const _hideAfterOnboard =
-                isOnboardingCompleted && hideAfterOnboard;
-              const shouldHideLink =
-                hideTillOnboard ||
-                _hideAfterOnboard ||
-                shadowHidden ||
-                requireAdminAccess;
-              const _requireAuth = !isLoggedIn && requireAuth;
+    <>
+      <aside className="hidden lg:flex">
+        <nav>
+          <div className="transition-all w-0 lg:w-[270px]" />
+          <div className="border rounded-lg flex flex-col gap-1 px-2 fixed py-4 z-10 bg-white dark:bg-slate-950 w-[270px]">
+            {sidebarLinks.map(
+              ({
+                key,
+                label,
+                Icon,
+                slug,
+                shadowHide,
+                adminAccess,
+                requireAuth,
+                showOnBoard,
+                hideAfterOnboard,
+              }) => {
+                const isCurrentPage = currentPageName === key.toLowerCase();
+                const requireAdminAccess = adminAccess && !isAdmin;
+                const shadowHidden = shadowHide && !isCurrentPage;
+                const hideTillOnboard = !isOnboardingCompleted && !showOnBoard;
+                const _hideAfterOnboard =
+                  isOnboardingCompleted && hideAfterOnboard;
+                const shouldHideLink =
+                  hideTillOnboard ||
+                  _hideAfterOnboard ||
+                  shadowHidden ||
+                  requireAdminAccess;
+                const _requireAuth = !isLoggedIn && requireAuth;
 
-              if (shouldHideLink) return;
-              return (
-                <SidebarLink
-                  key={`sidebar-link-${key}`}
-                  label={label}
-                  Icon={Icon}
-                  slug={slug}
-                  isActive={isCurrentPage}
-                  requireAuth={_requireAuth}
-                  disabled={_requireAuth}
-                />
-              );
-            },
-          )}
-        </div>
-      </nav>
-    </aside>
-  );
-};
-
-export const SidebarMobile = ({
-  sidebarOpen,
-  setSidebarOpen,
-  isAdmin,
-  isLoggedIn,
-  isOnboardingCompleted,
-}: {
-  sidebarOpen: boolean;
-  setSidebarOpen: Function;
-  isAdmin?: boolean;
-  isLoggedIn?: boolean;
-  isOnboardingCompleted: boolean;
-}) => {
-  const pathname = usePathname();
-  const currentPageName = pathname.split('/')[2];
-
-  return (
-    <SlideOver
-      isOpen={sidebarOpen}
-      closeSlideOver={() => setSidebarOpen(false)}
-    >
-      <div className="flex flex-col justify-between pb-20">
-        <div>
-          <div className="flex flex-col gap-3 pt-5 mb-5">
-            <div className="px-3">
-              <CodeWithUnclebigbayLogo />
-            </div>
+                if (shouldHideLink) return;
+                return (
+                  <SidebarLink
+                    key={`sidebar-link-${key}`}
+                    label={label}
+                    Icon={Icon}
+                    slug={slug}
+                    isActive={isCurrentPage}
+                    requireAuth={_requireAuth}
+                    disabled={_requireAuth}
+                  />
+                );
+              },
+            )}
           </div>
+        </nav>
+      </aside>
 
-          {sidebarLinks.map(
-            ({
-              key,
-              label,
-              Icon,
-              slug,
-              shadowHide,
-              adminAccess,
-              requireAuth,
-              showOnBoard,
-              hideAfterOnboard,
-            }) => {
-              const isCurrentPage = currentPageName === key.toLowerCase();
-              const hideTillOnboard = !isOnboardingCompleted && !showOnBoard;
-              const _hideAfterOnboard =
-                isOnboardingCompleted && hideAfterOnboard;
-              const shouldHideLink =
-                hideTillOnboard ||
-                _hideAfterOnboard ||
-                (shadowHide && !isCurrentPage) ||
-                (adminAccess && !isAdmin);
-              const showRequireAuthMessage = !isLoggedIn && requireAuth;
-              if (shouldHideLink) return;
-              return (
-                <SidebarLink
-                  key={`sidebar-link-${key}`}
-                  label={label}
-                  Icon={Icon}
-                  slug={slug}
-                  isActive={currentPageName === key.toLowerCase()}
-                  onClick={() =>
-                    setTimeout(() => {
-                      setSidebarOpen(false);
-                    }, 100)
-                  }
-                  requireAuth={showRequireAuthMessage}
-                />
-              );
-            },
-          )}
+      <SlideOver
+        isOpen={sidebarOpen}
+        closeSlideOver={() => setSidebarOpen(false)}
+      >
+        <div className="flex flex-col justify-between pb-20">
+          <div>
+            <div className="flex flex-col gap-3 pt-5 mb-5">
+              <div className="px-3">
+                <CodeWithUnclebigbayLogo />
+              </div>
+            </div>
+
+            {sidebarLinks.map(
+              ({
+                key,
+                label,
+                Icon,
+                slug,
+                shadowHide,
+                adminAccess,
+                requireAuth,
+                showOnBoard,
+                hideAfterOnboard,
+              }) => {
+                const isCurrentPage = currentPageName === key.toLowerCase();
+                const hideTillOnboard =
+                  isLoggedIn && !isOnboardingCompleted && !showOnBoard;
+                const _hideAfterOnboard =
+                  isOnboardingCompleted && hideAfterOnboard;
+                const shouldHideLink =
+                  hideTillOnboard ||
+                  _hideAfterOnboard ||
+                  (shadowHide && !isCurrentPage) ||
+                  (adminAccess && !isAdmin);
+                const showRequireAuthMessage = !isLoggedIn && requireAuth;
+                if (shouldHideLink) return;
+                return (
+                  <SidebarLink
+                    key={`sidebar-link-${key}`}
+                    label={label}
+                    Icon={Icon}
+                    slug={slug}
+                    isActive={currentPageName === key.toLowerCase()}
+                    onClick={() =>
+                      setTimeout(() => {
+                        setSidebarOpen(false);
+                      }, 100)
+                    }
+                    requireAuth={showRequireAuthMessage}
+                  />
+                );
+              },
+            )}
+          </div>
+          <div className="flex justify-center mt-10">
+            <IconButton
+              Icon={XMark}
+              size="xs"
+              appearance="secondary"
+              onClick={() => setSidebarOpen(false)}
+            />
+          </div>
         </div>
-        <div className="flex justify-center mt-10">
-          <IconButton
-            Icon={XMark}
-            size="xs"
-            appearance="secondary"
-            onClick={() => setSidebarOpen(false)}
-          />
-        </div>
-      </div>
-    </SlideOver>
+      </SlideOver>
+    </>
   );
 };
+
+// export const SidebarMobile = ({
+//   sidebarOpen,
+//   setSidebarOpen,
+//   isAdmin,
+//   isLoggedIn,
+//   isOnboardingCompleted,
+// }: {
+//   sidebarOpen: boolean;
+//   setSidebarOpen: Function;
+//   isAdmin?: boolean;
+//   isLoggedIn?: boolean;
+//   isOnboardingCompleted: boolean;
+// }) => {
+//   const pathname = usePathname();
+//   const currentPageName = pathname.split('/')[2];
+
+//   return (
+//     <SlideOver
+//       isOpen={sidebarOpen}
+//       closeSlideOver={() => setSidebarOpen(false)}
+//     >
+//       <div className="flex flex-col justify-between pb-20">
+//         <div>
+//           <div className="flex flex-col gap-3 pt-5 mb-5">
+//             <div className="px-3">
+//               <CodeWithUnclebigbayLogo />
+//             </div>
+//           </div>
+
+//           {sidebarLinks.map(
+//             ({
+//               key,
+//               label,
+//               Icon,
+//               slug,
+//               shadowHide,
+//               adminAccess,
+//               requireAuth,
+//               showOnBoard,
+//               hideAfterOnboard,
+//             }) => {
+//               const isCurrentPage = currentPageName === key.toLowerCase();
+//               const hideTillOnboard =
+//                 isLoggedIn && !isOnboardingCompleted && !showOnBoard;
+//               const _hideAfterOnboard =
+//                 isOnboardingCompleted && hideAfterOnboard;
+//               const shouldHideLink =
+//                 hideTillOnboard ||
+//                 _hideAfterOnboard ||
+//                 (shadowHide && !isCurrentPage) ||
+//                 (adminAccess && !isAdmin);
+//               const showRequireAuthMessage = !isLoggedIn && requireAuth;
+//               if (shouldHideLink) return;
+//               return (
+//                 <SidebarLink
+//                   key={`sidebar-link-${key}`}
+//                   label={label}
+//                   Icon={Icon}
+//                   slug={slug}
+//                   isActive={currentPageName === key.toLowerCase()}
+//                   onClick={() =>
+//                     setTimeout(() => {
+//                       setSidebarOpen(false);
+//                     }, 100)
+//                   }
+//                   requireAuth={showRequireAuthMessage}
+//                 />
+//               );
+//             },
+//           )}
+//         </div>
+//         <div className="flex justify-center mt-10">
+//           <IconButton
+//             Icon={XMark}
+//             size="xs"
+//             appearance="secondary"
+//             onClick={() => setSidebarOpen(false)}
+//           />
+//         </div>
+//       </div>
+//     </SlideOver>
+//   );
+// };
