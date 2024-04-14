aa8612d904b65ca6395d4cd609287ed94c76cff6
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-04-14 09:08:15 +0100

    Feat: add hackathonRegistration model

---
+++ src/models/hackathonRegistration.ts
@@ -0,0 +1,21 @@
+import mongoose, { model, models } from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const hackathonRegistrationSchema = new Schema(
+  {
+    hackathon: { type: Schema.ObjectId, ref: 'Hackathon', required: true },
+    student: { type: Schema.ObjectId, ref: 'Student', required: true },
+    isBanned: { type: Boolean },
+  },
+  {
+    timestamps: true,
+    collection: 'hackathonRegistrations',
+  },
+);
+
+const HackathonRegistration =
+  models.HackathonRegistration ||
+  model('HackathonRegistration', hackathonRegistrationSchema);
+
+export { HackathonRegistration };
