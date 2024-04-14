7ac4398256ee2952ac17b5fce9dea9d77915c2ba
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-04-14 09:12:42 +0100

    Feat: add HackathonSubmission model

---
+++ src/models/hackathonSubmission.ts
@@ -0,0 +1,30 @@
+import mongoose, { model, models } from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const hackathonSubmissionSchema = new Schema(
+  {
+    hackathon: { type: Schema.ObjectId, ref: 'Hackathon', required: true },
+    student: { type: Schema.ObjectId, ref: 'Student', required: true },
+    project: {
+      name: { type: String },
+      url: { type: String },
+      demoUrl: { type: String },
+      articleUrl: { type: String },
+      repositoryUrl: { type: String },
+      socialUrl: { type: String },
+      feedback: { type: String },
+    },
+    isBanned: { type: Boolean },
+  },
+  {
+    timestamps: true,
+    collection: 'hackathonSubmissions',
+  },
+);
+
+const HackathonSubmission =
+  models.HackathonSubmission ||
+  model('HackathonSubmission', hackathonSubmissionSchema);
+
+export { HackathonSubmission };
