0645cc5c4e27bd7017425c9a32f4911cc5506daf
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-04-26 16:52:08 +0100

    Feat: add hackathon widget to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,51 @@ import { HeroSection } from '@/components/molecules/home/HeroSection';
 import { SectionWrapper } from '@/components/molecules/home';
 import { Meteors } from '@/components/atoms/meteors';
 import { CommunityCTA } from '@/components/atoms/CommunityCTA';
+import { baseURL } from '../../frontend.config';
+import { headers } from 'next/headers';
+import { Hackathon } from '@/utils/types';
+import { HackathonWidget } from '@/components/molecules/home/HackathonWidget';
 
 export const metadata: Metadata = {
   title: 'Code with Unclebigbay',
   description: 'Learn to Code and Build Your Career',
 };
 
-export default function Home() {
+export async function getCurrentHackathon() {
+  try {
+    const url = `${baseURL}/api/hackathons/current-hackathon`;
+    const result = await fetch(url, {
+      headers: headers(),
+    });
+    const { hackathon } = await result.json();
+
+    const hackathonId = hackathon._id;
+    const isRegisteredUrl = `${baseURL}/api/hackathons/is-registered/${hackathonId}`;
+    const isRegisteredResult = await fetch(isRegisteredUrl, {
+      headers: headers(),
+    });
+
+    const { isRegistered } = await isRegisteredResult.json();
+    return { hackathon, isRegistered };
+  } catch (e: any) {
+    console.log({ message: e.message });
+  }
+}
+
+const Home = async () => {
+  const { hackathon, isRegistered } = (await getCurrentHackathon()) as {
+    hackathon: Hackathon;
+    isRegistered: boolean;
+  };
+
   return (
     <main>
       {/* Todo: figure out why ResponsiveWrapper isn't working intermittently */}
       <section className="flex flex-col gap-10 overflow-hidden">
-        <Navbar />
+        <div>
+          <Navbar />
+          <HackathonWidget hackathon={hackathon} isRegistered={isRegistered} />
+        </div>
         <SectionWrapper>
           <Meteors />
           <HeroSection />
@@ -45,4 +78,5 @@ export default function Home() {
       </section>
     </main>
   );
-}
+};
+export default Home;
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,6 @@
 import { signIn, signOut } from 'next-auth/react';
 
-export const handleAuthentication = (props: any) => {
+export const handleAuthentication = (props: { nextUrl?: string }) => {
   const { nextUrl } = props;
   signIn('github', {
     callbackUrl: nextUrl || `${window.location.origin}/dashboard`,
