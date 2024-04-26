00a632bc9f86e13785ab680a3020509b0c425d23
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-04-26 13:36:00 +0100

    Make audit log serverside

--- src/components/molecules/dashboard/activity-logs.tsx
+++ src/components/molecules/dashboard/activity-logs.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import Link from 'next/link';
 import { WhiteArea } from './white-area';
 import { DashboardSubheading } from './dashboard-subheading';
+import { Audits } from '@/utils/types';
 import { Button } from '@hashnode/matrix-ui';
-import useAudit from '@/components/hooks/useAudit';
 
 const renderTitle = (title: string) => (
   <h3 className="text-slate-600 ">{title}</h3>
@@ -55,73 +55,66 @@ const AuditTimeLine = ({
   );
 };
 
-const AuditTimeLineLoader = ({ count = 2 }: { count?: number }) => {
-  return (
-    <>
-      {Array(count)
-        .fill({})
-        .map((_, index) => {
-          return (
-            <div
-              className="relative pl-8 sm:pl-32 py-6 group"
-              key={`AuditTimeLineLoader-${index}`}
-            >
-              <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-slate-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                <time className="sm:absolute left-0 translate-y-0.5 w-20 h-6 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
-                <time className="w-28 h-6 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
-              </div>
-              <div className="w-24 h-5 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
-            </div>
-          );
-        })}
-    </>
-  );
-};
+// const AuditTimeLineLoader = ({ count = 2 }: { count?: number }) => {
+//   return (
+//     <>
+//       {Array(count)
+//         .fill({})
+//         .map((_, index) => {
+//           return (
+//             <div
+//               className="relative pl-8 sm:pl-32 py-6 group"
+//               key={`AuditTimeLineLoader-${index}`}
+//             >
+//               <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-slate-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+//                 <time className="sm:absolute left-0 translate-y-0.5 w-20 h-6 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
+//                 <time className="w-28 h-6 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
+//               </div>
+//               <div className="w-24 h-5 mb-3 sm:mb-0 bg-slate-100 rounded-full" />
+//             </div>
+//           );
+//         })}
+//     </>
+//   );
+// };
 
 export const ActivityLogs = ({
+  audits,
   defaultCount,
   hideShowMore,
-  loaderCount,
 }: {
+  audits: Audits;
   defaultCount?: number;
   loaderCount?: number;
   hideShowMore?: boolean;
 }) => {
-  const { audits, isFetching } = useAudit();
-
   return (
     <WhiteArea border>
       <div className="flex flex-col gap-5">
         <DashboardSubheading title="Activity" />
 
-        {isFetching ? (
-          <div className="-my-6">
-            <AuditTimeLineLoader count={loaderCount} />
-          </div>
-        ) : (
-          <div className="-my-6">
-            {audits
-              ?.slice(0, defaultCount)
-              .map(({ _id, createdAt, title, description, url }) => {
-                return (
-                  <AuditTimeLine
-                    key={_id}
-                    time={createdAt}
-                    title={title}
-                    description={description}
-                    url={url}
-                  />
-                );
-              })}
-            {!hideShowMore && (
-              <div className="relative pl-8 sm:pl-32 py-6 flex items-start">
-                <Button size="sm" appearance="secondary-slate" asChild>
-                  <Link href="/dashboard/activity">Show more</Link>
-                </Button>
-              </div>
-            )}
-          </div>
-        )}
+        <div className="-my-6">
+          {audits
+            ?.slice(0, defaultCount)
+            .map(({ _id, createdAt, title, description, url }) => {
+              return (
+                <AuditTimeLine
+                  key={_id}
+                  time={createdAt}
+                  title={title}
+                  description={description}
+                  url={url}
+                />
+              );
+            })}
+          {!hideShowMore && (
+            <div className="relative pl-8 sm:pl-32 py-6 flex items-start">
+              <Button size="sm" appearance="secondary-slate" asChild>
+                <Link href="/dashboard/activity">Show more</Link>
+              </Button>
+            </div>
+          )}
+        </div>
       </div>
     </WhiteArea>
   );
