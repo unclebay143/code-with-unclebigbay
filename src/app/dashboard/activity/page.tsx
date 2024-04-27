1c53c07b02afddd7dc65292aad1cfb95ce9f21f1
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-04-27 20:12:50 +0100

    Fix: audit error

--- src/app/dashboard/activity/page.tsx
+++ src/app/dashboard/activity/page.tsx
@@ -3,7 +3,8 @@ import { ActivityLogs } from '@/components/molecules/dashboard/activity-logs';
 import { getAllActivityAudits } from '@/utils/server.service';
 
 const Page = async () => {
-  const { audits } = await getAllActivityAudits();
-  return <ActivityLogs audits={audits} loaderCount={5} hideShowMore />;
+  const activityRes = await getAllActivityAudits();
+  const audits = activityRes?.audits;
+  return <ActivityLogs audits={audits!} loaderCount={5} hideShowMore />;
 };
 export default Page;
