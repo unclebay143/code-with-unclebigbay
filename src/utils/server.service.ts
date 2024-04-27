ffc2380b79d1ebf1810425c68487c7700cb7c5b2
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-04-27 21:19:16 +0100

    Debugging

--- src/utils/server.service.ts
+++ src/utils/server.service.ts
@@ -5,13 +5,20 @@ import { getServerSessionWithAuthOptions } from './auth-options';
 import { Countries, Country, Hackathon, Student, Students } from './types';
 import { Session } from 'next-auth';
 
+export const getCustomHeaders = () => {
+  const defaultHeaders = headers();
+  const customHeaders = new Headers(defaultHeaders);
+
+  return customHeaders;
+};
+
 export async function getCurrentStudent(): Promise<
   { student: Student } | undefined
 > {
   try {
     const url = `${baseURL}/api/auth/student`;
     const result = await fetch(url, {
-      headers: headers(),
+      headers: getCustomHeaders(),
       cache: 'force-cache',
     });
     const resultJson = await result.json();
@@ -75,7 +82,7 @@ export async function getAllActivityAudits(): Promise<
   try {
     const url = `${baseURL}/api/audits`;
     const result = await fetch(url, {
-      headers: headers(),
+      headers: getCustomHeaders(),
       cache: 'force-cache',
     });
 
@@ -99,7 +106,7 @@ export async function getCurrentHackathon(): Promise<
     const session = await getServerSessionWithAuthOptions();
     const url = `${baseURL}/api/hackathons/current-hackathon`;
     const result = await fetch(url, {
-      headers: headers(),
+      headers: getCustomHeaders(),
       cache: 'force-cache',
     });
     const { hackathon } = await result.json();
@@ -116,7 +123,7 @@ export async function getLeaderBoard() {
   try {
     const url = `${baseURL}/api/students/leaderboard`;
     const result = await fetch(url, {
-      headers: headers(),
+      headers: getCustomHeaders(),
       cache: 'no-cache',
     });
     const leaderboard = await result.json();
@@ -134,7 +141,7 @@ export async function getEnrolledCourses(): Promise<
     const url = `${baseURL}/api/courses/enroll`;
     const result = await fetch(url, {
       cache: 'force-cache',
-      headers: headers(),
+      headers: getCustomHeaders(),
     });
 
     if (!result.ok) {
@@ -152,7 +159,7 @@ export async function getAllHackathons() {
   try {
     const url = `${baseURL}/api/hackathons`; // isRegistered is derived from server
     const result = await fetch(url, {
-      headers: headers(),
+      headers: getCustomHeaders(),
       cache: 'force-cache',
     });
     const hackathons = await result.json();
@@ -167,7 +174,7 @@ export async function getHackathonBySlug(hackathonSlug: string) {
   try {
     const url = `${baseURL}/api/hackathons/${hackathonSlug}`;
     const result = await fetch(url, {
-      headers: headers(),
+      headers: getCustomHeaders(),
       cache: 'force-cache',
     });
 
@@ -176,10 +183,10 @@ export async function getHackathonBySlug(hackathonSlug: string) {
     const isRegisteredUrl = `${baseURL}/api/hackathons/is-registered/${hackathonId}`;
     const hasSubmittedUrl = `${baseURL}/api/hackathons/has-submitted/${hackathonId}`;
     const isRegisteredResult = await fetch(isRegisteredUrl, {
-      headers: headers(),
+      headers: getCustomHeaders(),
     });
     const hasSubmittedResult = await fetch(hasSubmittedUrl, {
-      headers: headers(),
+      headers: getCustomHeaders(),
     });
 
     const { isRegistered } = await isRegisteredResult.json();
