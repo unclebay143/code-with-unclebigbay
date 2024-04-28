d2e2d5877bd96bce0cd6d8487022e7316e09399a
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-04-28 07:30:41 +0100

    Debugging

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    // './node_modules/@hashnode/matrix-ui/dist/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@hashnode/matrix-ui/dist/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
