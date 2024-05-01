e4e59a91dd8b6074e401f6c13f051a17a5afcfe2
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-01 13:10:42 +0100

    Fixes

--- src/components/atoms/SidebarSlideOver.tsx
+++ src/components/atoms/SidebarSlideOver.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { SlideOver, SlideOverHeader } from './SlideOver';
+// import { SlideOver, SlideOverHeader } from './SlideOver';
 import { CodeWithUnclebigbayLogo } from './CodeWithUnclebigbayLogo';
 import { navLinks } from '@/utils/links';
-import { Button } from './Button';
 import Link from 'next/link';
 import { handleAuthentication } from '@/utils/auth';
 import { Session } from 'next-auth';
+import {
+  Button,
+  IconButton,
+  SlideoverPanel,
+  SlideoverPanelBody,
+  X,
+} from '@hashnode/matrix-ui';
 
 type Props = {
   isOpen: boolean;
@@ -15,56 +21,38 @@ type Props = {
 
 export const SidebarSlideOver = ({ isOpen, close, session }: Props) => {
   return (
-    <SlideOver isOpen={isOpen} closeSlideOver={close}>
-      <div className="flex items-center justify-between pl-5 pt-5 mb-5">
-        <CodeWithUnclebigbayLogo />
-        <SlideOverHeader borderless />
-      </div>
-
-      {navLinks.map(({ label, url }, index) => (
-        <div className="border-b ml-5" key={`big-screen-nav-links-${index}`}>
-          <Button
-            size="md"
-            appearance="link-secondary"
-            padding="none"
-            width="full"
-          >
-            <Link className="text-left w-full" href={url}>
-              {label}
-            </Link>
-          </Button>
+    <SlideoverPanel isOpen={isOpen} closePanel={close}>
+      <SlideoverPanelBody>
+        <div className="flex items-center justify-between">
+          <CodeWithUnclebigbayLogo />
+          <IconButton Icon={X} onClick={close} />
         </div>
-      ))}
-
-      <section className="sm:block w-[163px] ml-5">
-        {session ? (
-          <Button
-            size="md"
-            appearance="link-secondary"
-            width="full"
-            padding="none"
-          >
-            <Link
-              href="/dashboard/overview"
-              className="text-left block w-full text-black"
-            >
-              Dashboard
-            </Link>
-          </Button>
-        ) : (
-          <section className="">
-            <Button
-              size="md"
-              onClick={() => handleAuthentication()}
-              appearance="link-secondary"
-              width="full"
-              padding="none"
-            >
-              <span className="text-left block w-full text-black">Sign in</span>
+        <div className="flex flex-col gap-10 mt-10">
+          {session ? (
+            <Button size="md" appearance="link-slate" asChild>
+              <Link href="/dashboard/overview">Dashboard</Link>
             </Button>
-          </section>
-        )}
-      </section>
-    </SlideOver>
+          ) : (
+            <section className="">
+              <Button
+                size="md"
+                onClick={() => handleAuthentication()}
+                appearance="link-slate"
+              >
+                Sign in
+              </Button>
+            </section>
+          )}
+
+          {navLinks.map(({ label, url }, index) => (
+            <div key={`big-screen-nav-links-${index}`}>
+              <Button size="md" appearance="link-slate" asChild>
+                <Link href={url}>{label}</Link>
+              </Button>
+            </div>
+          ))}
+        </div>
+      </SlideoverPanelBody>
+    </SlideoverPanel>
   );
 };
