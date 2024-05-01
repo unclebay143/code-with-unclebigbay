3f24eff5acca776c062b0aad119bf79a84788136
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-01 10:43:39 +0100

    Type fix

--- src/components/atoms/SidebarSlideOver.tsx
+++ src/components/atoms/SidebarSlideOver.tsx
@@ -10,7 +10,7 @@ import { Session } from 'next-auth';
 type Props = {
   isOpen: boolean;
   close: () => void;
-  session: Session | null;
+  session?: Session | null;
 };
 
 export const SidebarSlideOver = ({ isOpen, close, session }: Props) => {
