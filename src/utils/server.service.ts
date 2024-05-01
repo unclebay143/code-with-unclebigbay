c944a8e24202278b73b57df0e0b1829d968ab085
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-01 08:07:52 +0100

    Move audit to db call to server function

--- src/utils/server.service.ts
+++ src/utils/server.service.ts
@@ -4,6 +4,8 @@ import { baseURL } from '../../frontend.config';
 import { getServerSessionWithAuthOptions } from './auth-options';
 import { Countries, Country, Hackathon, Student, Students } from './types';
 import { Session } from 'next-auth';
+import { Student as StudentModel } from '@/models/student';
+import { AuditTrail as AuditTrailModel } from '@/models/audit-trail';
 
 // https://www.reddit.com/r/nextjs/comments/16hzdsr/i_have_a_question_using_headers/
 export const getCustomHeaders = () => {
@@ -69,19 +71,18 @@ export async function getStudents(): Promise<GetStudentsResponse | undefined> {
     console.log(`Error from getStudents Error:- ${error}`);
   }
 }
-
+type GetAllActivityAuditsResponse = { audits: Audits };
 export async function getAllActivityAudits(): Promise<
-  { audits: Audits | [] } | undefined
+  GetAllActivityAuditsResponse | undefined
 > {
   try {
-    const url = `${baseURL}/api/audits`;
-    const result = await fetch(url, {
-      headers: headers(),
+    const session = await getServerSessionWithAuthOptions();
+    const student = await StudentModel.findOne({ email: session?.user.email });
+    const audits = await AuditTrailModel.find({ student: student.id }).sort({
+      createdAt: -1,
     });
 
-    if (!result.ok) return { audits: [] };
-    const audits = await result.json();
-    return audits;
+    return { audits: JSON.parse(JSON.stringify(audits)) };
   } catch (error) {
     console.log(`Error from getAllActivityAudits: Error:- ${error}`);
   }
