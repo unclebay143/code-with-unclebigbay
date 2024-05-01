6cd77175e2e60a84b74eae2addea02eb4692c4b5
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-01 07:40:02 +0100

    Fixing bug with vercel team

--- src/app/dashboard/activity/page.tsx
+++ src/app/dashboard/activity/page.tsx
@@ -3,7 +3,7 @@ import { getAllActivityAudits } from '@/utils/server.service';
 
 const Page = async () => {
   const activityRes = await getAllActivityAudits();
-  const audits = activityRes!.audits;
+  const audits = activityRes && activityRes.audits;
   return <ActivityLogs audits={audits} loaderCount={5} hideShowMore />;
 };
 export default Page;
