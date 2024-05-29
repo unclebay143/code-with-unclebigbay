b3bbc22af3581fab2d72ee508ff8de733e6afd5b
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-05-29 19:09:54 +0100

    Open graph update

--- src/app/dashboard/hackathons/[slug]/page.tsx
+++ src/app/dashboard/hackathons/[slug]/page.tsx
@@ -15,11 +15,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 
   return {
+    title: `Hackathon - ${hackathonRes.hackathon.title}`,
+    description: hackathonRes.hackathon.brief,
     openGraph: {
       images: [hackathonRes.hackathon.coverImage],
     },
-    title: `Hackathon - ${hackathonRes.hackathon.title}`,
-    description: hackathonRes.hackathon.brief,
   };
 }
 
