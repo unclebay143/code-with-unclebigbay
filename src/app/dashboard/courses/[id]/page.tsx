f605c2f7af3c757fe9cfae157bcf0cce2c212c39
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-06-04 19:18:53 +0100

    Refactor: course page to show metadata

--- src/app/dashboard/courses/[id]/page.tsx
+++ src/app/dashboard/courses/[id]/page.tsx
@@ -1,13 +1,12 @@
 import { Metadata } from 'next';
 import Course from './course';
-import { Course as CourseType } from '@/utils/types';
 
 type Props = { params: { id: string } };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const courseSlug = params.id;
   const prod = process.env.NODE_ENV === 'production';
-  const baseUrl = !prod
+  const baseUrl = prod
     ? 'https://www.codewithunclebigbay.com'
     : 'http://localhost:3001';
   const url = baseUrl + '/api/courses/' + courseSlug;
