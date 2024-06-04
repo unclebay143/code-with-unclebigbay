5adcc5fffa9f294f88e9f99b04c9d0978e0758f8
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-06-04 19:18:22 +0100

    Refactor: course page to show metadata

--- src/app/dashboard/courses/[id]/page.tsx
+++ src/app/dashboard/courses/[id]/page.tsx
@@ -6,16 +6,19 @@ type Props = { params: { id: string } };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const courseSlug = params.id;
-  const res = await fetch(
-    'https://www.codewithunclebigbay.com/api/courses/' + courseSlug,
-  );
-  const data: CourseType = await res.json();
+  const prod = process.env.NODE_ENV === 'production';
+  const baseUrl = !prod
+    ? 'https://www.codewithunclebigbay.com'
+    : 'http://localhost:3001';
+  const url = baseUrl + '/api/courses/' + courseSlug;
+  const res = await fetch(url);
+  const { course } = await res.json();
 
   return {
-    title: data.title,
-    description: data.brief,
+    title: course.title,
+    description: course.brief,
     openGraph: {
-      images: [data.coverImageUrl],
+      images: [course.coverImageUrl],
     },
   };
 }
