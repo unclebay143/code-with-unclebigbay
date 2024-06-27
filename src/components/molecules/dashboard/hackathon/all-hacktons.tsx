fa5a59c53a5df0360129f45980ae85d8e334ce9f
eedrisofficial <eedrisofficial.gmail.com>
2024-06-26 22:14:25 -0400

    update

--- src/components/molecules/dashboard/hackathon/all-hacktons.tsx
+++ src/components/molecules/dashboard/hackathon/all-hacktons.tsx
@@ -4,79 +4,89 @@ import Image from 'next/image';
 import Link from 'next/link';
 import './style.css';
 
+interface HackathonType {
+  id: number;
+  name: string;
+  title: string;
+  description: string;
+  coverImage: string;
+  profileImage: string;
+}
 export default function AllHackathons() {
-  const [hackathons, setHackathons] = useState(allHackathons);
+  const [hackathons, setHackathons] = useState<HackathonType[]>(allHackathons);
 
   return (
     <section>
       <div className="grid grid-cols-3 gap-4 justify-start items-start mt-3">
         {hackathons && hackathons.length > 0 ? (
-          hackathons.map((hackathon, index) => (
-            <section
-              key={index}
-              className="flex flex-col  gap-3 border border-slate-200 rounded-md hover:border-slate-400  w-[300px] h-[350px] odd:bg-[#FFF] even:bg-slate-50 pb-6"
-            >
-              <Link
-                href={`/dashboard/hackathons/hackathon-name/submissions/${index}`}
-                className="h-32 w-full overflow-hidden cursor-pointer "
+          hackathons.map(
+            ({ id, name, title, description, coverImage, profileImage }) => (
+              <section
+                key={id}
+                className="flex flex-col  gap-3 border border-slate-200 rounded-md hover:border-slate-400  w-[300px] h-[350px] odd:bg-[#FFF] even:bg-slate-50 pb-6"
               >
-                <Image
-                  src={hackathon.coverimage}
-                  alt={hackathon.name}
-                  width={500}
-                  height={500}
-                  priority
-                  className="rounded-t-md h-full w-full"
-                />
-              </Link>
-              <div className="flex flex-col gap-5 p-2 ">
-                <div className="flex flex-col gap-3">
-                  <h1 className="text-slate-800 text-md truncate-two-lines font-medium">
-                    {hackathon.title}
-                  </h1>
-                  <p className="text-slate-400 text-sm truncate-three-lines ">
-                    {hackathon.description}
-                  </p>
-                </div>
-                <section className="flex justify-between items-center">
-                  <div className="flex gap-2 items-center text-slate-500 text-xs">
-                    <p>By</p>
-                    <div className="h-7 w-7 overflow-hidden rounded-full hover:opacity-90">
-                      <Image
-                        src={hackathon.profileimage}
-                        alt={hackathon.name}
-                        width={90}
-                        height={90}
-                        priority
-                      />
-                    </div>
-                    <p> {hackathon.name}</p>
+                <Link
+                  href={`/dashboard/hackathons/hackathon-name/submissions/${id}`}
+                  className="h-32 w-full overflow-hidden cursor-pointer "
+                >
+                  <Image
+                    src={coverImage}
+                    alt={name}
+                    width={500}
+                    height={500}
+                    priority
+                    className="rounded-t-md h-full w-full"
+                  />
+                </Link>
+                <div className="flex flex-col gap-5 p-2 ">
+                  <div className="flex flex-col gap-3">
+                    <h1 className="text-slate-800 text-md truncate-two-lines font-medium">
+                      {title}
+                    </h1>
+                    <p className="text-slate-400 text-sm truncate-three-lines ">
+                      {description}
+                    </p>
                   </div>
-                  <a
-                    target="_blank"
-                    rel="noopener"
-                    href="https://dub.sh/LJOPAON"
-                    className="text-slate-400 flex justify-end cursor-pointer hover:text-slate-600 px-2"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="size-5"
+                  <section className="flex justify-between items-center">
+                    <div className="flex gap-2 items-center text-slate-500 text-xs">
+                      <p>By</p>
+                      <div className="h-7 w-7 overflow-hidden rounded-full hover:opacity-90">
+                        <Image
+                          src={profileImage}
+                          alt={name}
+                          width={90}
+                          height={90}
+                          priority
+                        />
+                      </div>
+                      <p> {name}</p>
+                    </div>
+                    <a
+                      target="_blank"
+                      rel="noopener"
+                      href="https://dub.sh/LJOPAON"
+                      className="text-slate-400 flex justify-end cursor-pointer hover:text-slate-600 px-2"
                     >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                      />
-                    </svg>
-                  </a>
-                </section>
-              </div>
-            </section>
-          ))
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="size-5"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                        />
+                      </svg>
+                    </a>
+                  </section>
+                </div>
+              </section>
+            ),
+          )
         ) : (
           <div className="text-xl text-center text-slate-600">
             Hello, Our first hackathon will kick off soon...
--- src/utils/consts/all-hackathons.ts
+++ src/utils/consts/all-hackathons.ts
@@ -1,242 +1,252 @@
-export const allHackathons = [
+export type Hackathon = {
+  id: number;
+  name: string;
+  title: string;
+  description: string;
+  coverImage: string;
+  profileImage: string;
+};
+
+export type Hackathons = Hackathon[];
+export const allHackathons: Hackathons = [
   {
     id: 1,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses.  the building block of the web',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages.the building block of the web',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 2,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 3,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses. the building block of the web',
     description:
       'Master JavaScript basics and add interactivity to your websites with dynamic scripting.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 4,
     name: 'Samuel Ayodele',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Master the modern utility-first CSS framework and streamline your web development workflow.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 5,
     name: 'Samuel Ayodele',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Discover React, a popular JavaScript library for building user interfaces, and create dynamic web applications.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 6,
     name: 'Samuel Ayodele',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Explore Next.js, a powerful React framework for building server-side rendered web applications.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 7,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 8,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 9,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 10,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 11,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages.the building block of the web',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 12,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 13,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages. the building block of the web',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 14,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 15,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages. the building block of the web',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 16,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 18,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 19,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
   {
     id: 19,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Learn the fundamentals of HTML, the building block of the web, and create your first web pages. the building block of the web',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719374595684/f7b4f66a-f606-4d9b-a3b4-410cdee76806.png',
   },
   {
     id: 20,
     name: 'Idris Haruna',
-    profileimage:
+    profileImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1677222800340/7FWlpF0aT.jpeg',
     title:
       'Build a Landing Page, Boost Visibility && create your first web pages for businesses',
     description:
       'Dive into CSS and learn how to style your web pages to make them visually appealing.',
-    coverimage:
+    coverImage:
       'https://cdn.hashnode.com/res/hashnode/image/upload/v1719317574659/2c66b4f7-b94a-4c24-8bef-6f51d3d35e69.png',
   },
 ];
