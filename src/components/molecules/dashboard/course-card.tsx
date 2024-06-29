bbb6db76ad4efa5ce06afabcb47499f2a3d3ef74
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-06-29 18:09:40 +0100

    update card design

--- src/components/molecules/dashboard/course-card.tsx
+++ src/components/molecules/dashboard/course-card.tsx
@@ -72,22 +72,24 @@ export const CourseCard = ({ layout = 'grid', course }: CourseCardProps) => {
             </div>
           </div>
           <section className="p-5 flex flex-col gap-3 justify-between">
+            <div className="w-full flex justify-between">
+              {viewTime ? (
+                <span className="py-1 px-2 whitespace-nowrap rounded-full bg-slate-100 text-xs font-semibold">
+                  {formatTime(viewTime)}
+                </span>
+              ) : null}
+              {showNewBadge && <Badge theme="green">New</Badge>}
+            </div>
             <div className="flex flex-col gap-3">
               <div className="flex flex-col gap-2 items-start justify-between">
-                <div className="">
+                <div>
                   <h3
                     title={title}
                     className="inline text-gray-700 font-medium hover:text-slate-800 text-lg"
                   >
                     {title}
-                  </h3>{' '}
-                  {showNewBadge && <Badge theme="green">New</Badge>}
+                  </h3>
                 </div>
-                {viewTime ? (
-                  <span className="py-1 px-2 whitespace-nowrap rounded-full bg-slate-100 text-xs font-semibold">
-                    {formatTime(viewTime)}
-                  </span>
-                ) : null}
               </div>
               <p
                 className="text-slate-500 line-clamp-3"
