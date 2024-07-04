79e37996c86b231a8a07789288162fb83b90e799
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-07-04 19:53:43 +0100

    fix

--- src/utils/server.service.ts
+++ src/utils/server.service.ts
@@ -237,7 +237,66 @@ export async function getEnrolledCourses(): Promise<
   return { enrolledCourses: JSON.parse(JSON.stringify(enrolledCourses)) };
 }
 
+// export async function getAllHackathons() {
+//   const session = await getServerSessionWithAuthOptions();
+//   await connectViaMongoose();
+
+//   let student;
+
+//   if (session) {
+//     student = await StudentModel.findOne({ email: session.user.email });
+//   }
+
+//   const hackathons = await HackathonModel.aggregate([
+//     {
+//       $lookup: {
+//         from: 'hackathonRegistrations',
+//         localField: '_id',
+//         foreignField: 'hackathon',
+//         as: 'participants',
+//       },
+//     },
+//     {
+//       $addFields: {
+//         participantCount: { $size: '$participants' },
+//       },
+//     },
+//     {
+//       $sort: { createdAt: -1 }, // Sort by createdAt in descending order (newest first)
+//     },
+//     {
+//       $project: {
+//         _id: 1,
+//         title: 1,
+//         startDate: 1,
+//         endDate: 1,
+//         coverImage: 1,
+//         desktopCoverImage: 1,
+//         tags: 1,
+//         brief: 1,
+//         isActive: 1,
+//         status: 1,
+//         slug: 1,
+//         participantCount: 1,
+//         ...(student && {
+//           isRegistered: { $gt: [{ $size: '$participants' }, 0] },
+//         }), // Check if participants exist
+//       },
+//     },
+//   ]);
+
+//   return { hackathons: JSON.parse(JSON.stringify(hackathons)) };
+// }
+
 export async function getAllHackathons() {
+  // const url = `${baseURL}/api/hackathons`; // isRegistered is derived from server
+  // const result = await fetch(url, {
+  //   headers: {
+  //     Cookie: await getCookie(),
+  //   },
+  //   cache: 'no-store',
+  // });
+  // const hackathons = await result.json();
   const session = await getServerSessionWithAuthOptions();
   await connectViaMongoose();
 
@@ -254,6 +313,12 @@ export async function getAllHackathons() {
         localField: '_id',
         foreignField: 'hackathon',
         as: 'participants',
+        ...(student && {
+          pipeline: [
+            { $match: { student: student._id } }, // Filter registrations by student ID
+            { $project: { _id: 0, fullName: 1, status: 1 } },
+          ],
+        }),
       },
     },
     {
