de5c38382040d5929b6619cf9a0dc4f27118abb1
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-07-24 17:42:10 +0100

    Milestone: release members option in hackathon submission

--- src/components/hooks/useStudent.ts
+++ src/components/hooks/useStudent.ts
@@ -1,16 +1,19 @@
 import { fetchStudentByUsername } from '@/utils/services/student';
+import { Student } from '@/utils/types';
 import { useQuery } from '@tanstack/react-query';
 
 const useStudent = (username: string) => {
   const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ['studentData'],
+    queryKey: ['studentData', username],
     refetchOnWindowFocus: false,
     refetchOnReconnect: true,
     refetchOnMount: false,
+    retry: false,
+    enabled: !!username, // prevent immediate searching on mount
     queryFn: () => fetchStudentByUsername(username), // add type here i.e as Student
   });
 
-  return { data, isFetching, error, isPending };
+  return { data: data as Student, isFetching, error, isPending };
 };
 
 export default useStudent;
