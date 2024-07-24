a2a0d24330d8dd0fd8cc7d4f460b65de48971632
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-07-24 17:42:10 +0100

    Milestone: release members option in hackathon submission

--- src/components/ui/StudentSearchField.tsx
+++ src/components/ui/StudentSearchField.tsx
@@ -3,34 +3,24 @@ import { z } from 'zod';
 import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import {
   Avatar,
-  Badge,
-  InputField,
   MatrixImage as Image,
   SearchContentField,
 } from '@hashnode/matrix-ui';
 import debounce from 'lodash/debounce';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Student, Students } from '@/utils/types';
+import { Students } from '@/utils/types';
 import { DEFAULT_PROFILE_PHOTO } from '@/utils';
+import useStudent from '../hooks/useStudent';
+import { toast } from 'sonner';
 
 const StudentSearchResultSkeleton = ({ count = 3 }: { count?: number }) => (
-  <>
-    {Array(count)
-      .fill(true)
-      .map((_, index) => (
-        <div
-          className="animate-pulse w-full flex items-center py-2 h-[72px] pl-2 gap-3"
-          // eslint-disable-next-line react/no-array-index-key
-          key={`StudentSearchResultSkeleton-${index}`}
-        >
-          <div className="rounded-full border border-slate-50 overflow-hidden h-10 w-10 bg-slate-100" />
-          <div className="flex flex-col flex-1 mt-2 md:(mt-0)">
-            <div className="rounded-lg h-4 w-4/12 bg-slate-100" />
-            <div className="mt-3 rounded-lg h-3 w-5/12 bg-slate-100" />
-          </div>
-        </div>
-      ))}
-  </>
+  <div className="animate-pulse w-full flex items-center py-1 h-10 pl-2 gap-3">
+    <div className="rounded-full border border-slate-50 overflow-hidden h-10 w-10 bg-slate-100" />
+    <div className="flex flex-col flex-1 mt-2 md:(mt-0)">
+      <div className="rounded-lg h-3 w-4/12 bg-slate-100" />
+      <div className="mt-2 rounded-lg h-3 w-5/12 bg-slate-100" />
+    </div>
+  </div>
 );
 
 type SearchTermSchema = z.infer<typeof searchTermSchema>;
@@ -41,12 +31,11 @@ const searchTermSchema = z.object({
 export const StudentSearchField = ({
   setSelection,
   exclusion, // i.e already selected users
+  maxSelection,
 }: {
   setSelection: Function;
-  exclusion: Pick<
-    Student,
-    '_id' | 'fullName' | 'photo' | 'username' | 'email'
-  >[];
+  exclusion: Students;
+  maxSelection?: number;
 }) => {
   const [searchTermChanged, setSearchTermChanged] = useState<boolean>(false);
 
@@ -57,47 +46,40 @@ export const StudentSearchField = ({
     },
   });
 
-  const searchResults: Pick<
-    Student,
-    '_id' | 'fullName' | 'photo' | 'username' | 'email'
-  >[] = [
-    {
-      email: '',
-      _id: '',
-      fullName: 'Ayodele Samuel Adebayo',
-      username: 'unclebay143',
-      photo: '',
-    },
-  ];
-  const isSearchingUser = false;
   const searchTerm = useWatch({
     control,
     name: 'searchTerm',
   });
+
+  const { data: searchResult, isPending: isStudentSearchPending } =
+    useStudent(searchTerm);
+
+  // Exclude already selected users from result if email or id matches
+  const alreadySelected = exclusion?.some(
+    (student) => (student?.username ?? '') === searchResult?.username,
+  );
 
   const clearSearchTermInput = () => {
     setValue('searchTerm', '');
   };
 
   const hasSearchTerm = searchTerm !== '';
-  const isNotSearchingHashnodeUsers = !isSearchingUser;
+  const isNotSearchingHashnodeUsers = !isStudentSearchPending;
 
-  // Exclude already selected users from result if email or id matches
-  const filteredResult = searchResults?.filter(
-    (result) =>
-      !exclusion?.some((user) => (user?.username ?? '') === result.username),
-  );
+  const hasReachedMaxSelection = maxSelection === exclusion.length;
 
-  console.log(filteredResult);
-
-  const hasSearchResults = filteredResult && filteredResult.length > 0;
+  const hasSearchResults = !!searchResult && !alreadySelected;
   const hasNoSearchResult =
-    hasSearchTerm && isNotSearchingHashnodeUsers && !hasSearchResults;
+    hasSearchTerm &&
+    isNotSearchingHashnodeUsers &&
+    !hasSearchResults &&
+    !alreadySelected;
 
-  const showInitialLoader = isSearchingUser || searchTermChanged;
+  const showLoader =
+    searchTerm && (isStudentSearchPending || searchTermChanged);
+
   const showSearchResults =
     !searchTermChanged && hasSearchTerm && hasSearchResults; // when search term hasn't change and there's input value and search result
-  const showLoadMoreLoader = isSearchingUser;
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleSearchTermChange = useCallback(
@@ -117,10 +99,13 @@ export const StudentSearchField = ({
 
   useEffect(() => {
     // Invalidate searchTermChanged for searchTerm with results
-    if (searchTermChanged && !isSearchingUser) {
+    if (searchTermChanged && !isStudentSearchPending) {
       setSearchTermChanged(false);
     }
-  }, [isSearchingUser, searchTermChanged]);
+  }, [isStudentSearchPending, searchTermChanged]);
+
+  const absoluteWrapper =
+    'w-full absolute top-10 z-10 bg-white shadow-xl py-2 border rounded-lg';
 
   return (
     <section className="relative">
@@ -135,58 +120,66 @@ export const StudentSearchField = ({
         />
       </section>
 
-      {/* {showInitialLoader && (
-        <section className="w-full absolute top-20 z-10 bg-white shadow-xl py-2 border border-slate-300 rounded-xl">
+      {showLoader && (
+        <section className={absoluteWrapper}>
           <StudentSearchResultSkeleton />
         </section>
-      )} */}
+      )}
 
       {hasNoSearchResult && (
-        <section className="w-full absolute top-20 z-10 bg-white shadow-xl py-2 border border-slate-300 rounded-xl">
-          <p className="text-center text-sm text-slate-600 dark:text-slate-300">
-            No user matches your search. Try searching their exact username or
-            name.
+        <section className={absoluteWrapper}>
+          <p className="text-center text-sm text-slate-500">
+            No user matches your search.
+          </p>
+        </section>
+      )}
+
+      {alreadySelected && (
+        <section className={absoluteWrapper}>
+          <p className="text-center text-sm text-slate-500">
+            <span className="font-medium">&apos;{searchTerm}&apos;</span> is
+            already added as member.
           </p>
         </section>
       )}
 
       {showSearchResults && (
-        <section className="w-full absolute top-90 z-10 bg-white shadow-xl py-2 border rounded-lg">
+        <section className="w-full absolute top-90 z-10 bg-white shadow-xl py-3 border rounded-lg">
           {/* user search result  */}
           <div className="max-h-[400px]">
-            {filteredResult?.map((student) => (
-              <button
-                key={`StudentSearch-${student.username}`}
-                type="button"
-                onClick={() => {
-                  clearSearchTermInput();
-                  setSelection((prevSelection: any) => [
-                    ...prevSelection,
-                    student,
-                  ]);
-                }}
-                className="w-full text-left cursor-pointer flex justify-between items-center py-1 h-10 px-3 gap-3 hover:bg-slate-50"
-                disabled={!!student}
-              >
-                <section className="flex justify-between items-center gap-3">
-                  <Avatar size="sm">
-                    <Image
-                      src={student?.photo || DEFAULT_PROFILE_PHOTO}
-                      alt={student?.fullName || 'Default profile photo'}
-                      fill
-                    />
-                  </Avatar>
-                  <section>
-                    <h3 className="text-sm font-semibold text-slate-700 dark:text-slate-200">
-                      {student.fullName}
-                    </h3>
-                    <p className="text-sm text-slate-500">
-                      @{student.username}
-                    </p>
-                  </section>
+            <button
+              key={`StudentSearch-${searchResult.username}`}
+              type="button"
+              onClick={() => {
+                if (hasReachedMaxSelection)
+                  return toast.error('Maximum number of members reached.');
+
+                clearSearchTermInput();
+                setSelection((prevSelection: any) => [
+                  ...prevSelection,
+                  searchResult,
+                ]);
+              }}
+              className="w-full text-left cursor-pointer flex justify-between items-center py-1 h-10 px-3 gap-3 hover:bg-slate-50"
+            >
+              <section className="flex justify-between items-center gap-3">
+                <Avatar size="sm">
+                  <Image
+                    src={searchResult.photo || DEFAULT_PROFILE_PHOTO}
+                    alt={searchResult?.fullName || 'Default profile photo'}
+                    fill
+                  />
+                </Avatar>
+                <section>
+                  <h3 className="text-sm font-semibold text-slate-700 dark:text-slate-200">
+                    {searchResult.fullName}
+                  </h3>
+                  <p className="text-sm text-slate-500">
+                    @{searchResult.username}
+                  </p>
                 </section>
-              </button>
-            ))}
+              </section>
+            </button>
 
             {/* {showLoadMoreLoader && <StudentSearchResultSkeleton />} */}
           </div>
