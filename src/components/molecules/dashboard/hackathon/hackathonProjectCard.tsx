2e38464691fb95f6fbc5a356a9d87003e8c6cce2
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-07-25 15:40:46 +0100

    Improve card

--- src/components/molecules/dashboard/hackathon/hackathonProjectCard.tsx
+++ src/components/molecules/dashboard/hackathon/hackathonProjectCard.tsx
@@ -33,13 +33,14 @@ export default function HackathonProjectCard(props: Props) {
     <Link
       key={_id}
       href={`/dashboard/hackathons/${slug}/submissions/${_id}`}
-      className="border  rounded-xl overflow-hidden transition-colors duration-200 ease-in-out hover:border-slate-300"
+      className="flex flex-col border rounded-xl overflow-hidden transition-colors duration-200 ease-in-out hover:border-slate-300"
     >
-      <div className="relative">
+      <div className="relative h-[180px]">
         <YTVideo ytVideoId={project?.demoUrl ?? ''} removeRounded />
         <div className="absolute z-10 inset-0 w-full h-full" />
       </div>
-      <div className="flex flex-col gap-5 p-4 pb-[14px]">
+
+      <div className="flex flex-col flex-1 justify-between gap-5 p-4 pb-[14px]">
         <div className="flex flex-col gap-3">
           <h1 className="text-gray-700 text-md line-clamp-2 font-medium">
             {project?.name}
@@ -53,18 +54,20 @@ export default function HackathonProjectCard(props: Props) {
         </div>
         <div className="flex flex-col gap-1">
           <div className="flex items-center gap-1 text-slate-500 text-xs">
-            <Avatar size="xs">
-              <Image
-                className="object-cover rounded-full h-5 w-5"
-                src={student?.photo || DEFAULT_PROFILE_PHOTO}
-                alt="participant"
-                width={24}
-                height={24}
-              />
-            </Avatar>
-            <div className="flex items-center">
-              <p>by</p>&nbsp;
-              <p className="truncate max-w-[200px]">{student?.fullName}</p>
+            <div className="flex items-center gap-0.5">
+              <p>by</p>
+              <Avatar size="xs">
+                <Image
+                  className="object-cover rounded-full h-5 w-5"
+                  src={student?.photo || DEFAULT_PROFILE_PHOTO}
+                  alt="participant"
+                  width={24}
+                  height={24}
+                />
+              </Avatar>
+              <p className="truncate text-xs max-w-[200px]">
+                {student?.fullName}
+              </p>
             </div>
           </div>
           <div className="flex items-end justify-between">
