07b9cc10666bb2b5e4aa0407e70cfc179cb31ca6
Unclebigbay <ayodelesamueladebayo143@gmail.com>
2024-07-26 10:36:10 +0100

    Release: hackathons project pages (#194)
    * Fix: copy
    
    * Feat: add audit for hackathon submission members
    
    * implementation for view all hackathons page (#164)
    
    * implementation for view all hackathons page
    
    * changes effected
    
    * description added
    
    * update
    
    * Redesign hackathon project cards
    
    * all corrections effected
    
    * corrections implemented
    
    * updates on submitted hackathons
    
    * Feat: add path of hackathon to dashboard
    
    * names variable corrections implemented
    
    * error fixed
    
    * key error fixed
    
    * previewed errors fixed
    
    * Feat: add view hackathon submissions link
    
    * Patch: ui
    
    * Link submission to hackathon story page
    
    * corrections implemented
    
    * updates on hackathon-project dummy data
    
    * implementation of hackathon project preview page
    
    * conflict fixed
    
    * updates on hackathon preview implementation
    
    * updates on preview project submitted date
    
    * updates on project share button
    
    * updates
    
    * update on share url
    
    * GET endpoint for hackathon submitted project done
    
    * getbyid endpoint done
    
    * consuming of hackathon submitted projects implementation
    
    * consumed hackathon submitted  projects get endpoint
    
    * updates on get endpoint for submitted hackathons projects
    
    * updates on hackathon project preview
    
    * updated student details to hackathons project and preview page
    
    * error fixed
    
    * error fixed
    
    * Chore: design improvement
    
    * corrections effected
    
    * Chore: fix broken items
    
    * all hackathon-projects endpoints moved to server side
    
    * Chore: copy fixes
    
    * all correction implemented
    
    * dynamic messages for share hackathon projects to social media implemented
    
    * not found page for hackathon projects implemented
    
    * patch html render
    
    * patch
    
    * hackathon project id error message fixed
    
    * Merge branch 'idris/view-all-hackathons' of https://github.com/unclebay143/code-with-unclebigbay into idris/view-all-hackathons
    
    * conflict fixed
    
    * refactor: link section
    
    * hackathon project members implemented
    
    * type error for hackathon project membes fixed
    
    * error fixed
    
    * patches
    
    * Improve card
    
    * Release: hackathon project pages
    
    ---------
    
    Co-authored-by: eedrisofficial <eedrisofficial.gmail.com>
    Co-authored-by: Unclebigbay <ayodelesamueladebayo143@gmail.com>
    
    ---------
    
    Co-authored-by: Haruna Idris <eedrisharuna3@gmail.com>

--- src/components/molecules/dashboard/modal-wrapper.tsx
+++ src/components/molecules/dashboard/modal-wrapper.tsx
@@ -47,9 +47,9 @@ export const ModalWrapper = ({
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed z-[9999] inset-0 animate-fade-in bg-gray-100 bg-opacity-50 backdrop-blur-md" />
+        <Dialog.Overlay className="fixed z-[30] inset-0 animate-fade-in bg-gray-100 bg-opacity-50 backdrop-blur-md" />
         <Dialog.Content
-          className={`border z-[9999] fixed bg-white dark:bg-slate-950 top-[50%] left-[50%] w-full p-5 rounded-md -translate-y-2/4 -translate-x-2/4`}
+          className={`border z-[30] fixed bg-white dark:bg-slate-950 top-[50%] left-[50%] w-full p-5 rounded-md -translate-y-2/4 -translate-x-2/4`}
           style={{ maxWidth }}
         >
           {children}
