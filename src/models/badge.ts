7bc462b5fcc864368e7ebdf36450fd25a498f8f1
eedrisofficial <eedrisofficial.gmail.com>
2024-08-08 02:03:53 -0400

    model collections seperated into diff files

--- src/models/badge.ts
+++ src/models/badge.ts
@@ -1,28 +1,18 @@
 import mongoose, { model, models } from 'mongoose';
 const Schema = mongoose.Schema;
 
-const studentBadgeSchema = new Schema(
-  {
-    student: { type: Schema.Types.ObjectId, ref: 'Student', required: true },
-    badge: { type: String, required: true },
-    achievedDate: { type: Date },
-  },
-  {
-    timestamps: true,
-    collection: 'studentBadges',
-  },
-);
-
 const badgeSchema = new Schema(
   {
     name: { type: String, required: true },
     displayName: { type: String, required: true },
     description: { type: String },
     url: { type: String },
-    badge: {
-      label: { type: String },
-      theme: { type: String },
-    },
+    badge: [
+      {
+        label: { type: String },
+        theme: { type: String },
+      },
+    ],
   },
   {
     timestamps: true,
@@ -30,8 +20,6 @@ const badgeSchema = new Schema(
   },
 );
 
-const StudentBadge =
-  models.StudentBadge || model('StudentBadge', studentBadgeSchema);
 const Badge = models.Badge || model('Badge', badgeSchema);
 
-export { StudentBadge, Badge };
+export { Badge };
